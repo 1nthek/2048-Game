200964970d76b49576cac2424b74a453698eddbe
IN DUCK KANG <idk0919@naver.com>
2021-02-25 00:43:34 +0900

    feat: 2048 game

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AboveGame from './components/AboveGame';
+import Game from './components/Game';
+import Header from './components/Header';
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
-  return <div className="container" />;
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useLocalStorage('bestScore', 0);
+
+  useEffect(() => {
+    if (score > bestScore) {
+      setBestScore(score);
+    }
+  });
+
+  return (
+    <div className="container">
+      <Header score={score} bestScore={bestScore} />
+      <AboveGame />
+      <Game setScore={setScore} />
+    </div>
+  );
 }
 
 export default App;
